6bc9cacc7f4b4ca960a7658846ab686db6ab46a0
EwokOzwok <eozmat@albany.edu>
2026-01-04 13:02:29 -0500

    removed subpage hosting base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/rolodex_generator/',
+  base: '/',
   plugins: [react()],
 });
